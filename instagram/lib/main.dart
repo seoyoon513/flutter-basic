bbffb1f3719ce4fd3d4b31bfd6e323a033876f8a
seoyoon513 <syoonlee513@gmail.com>
2023-03-03 14:34:31 +0900

    [fix] #1 OS Error: CERTIFICATE_VERIFY_FAILED 에러 해결

--- instagram/lib/main.dart
+++ instagram/lib/main.dart
@@ -1,8 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(MyApp());
+}
+
+class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext? context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+  }
 }
 
 class MyApp extends StatelessWidget {
