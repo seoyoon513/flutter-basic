5684c443b3d955474515f1246705a9a4df0402a7
seoyoon513 <syoonlee513@gmail.com>
2023-03-03 15:17:41 +0900

    [feat] #1 feed에 imageUrl 변수 추가

--- instagram/lib/feed.dart
+++ instagram/lib/feed.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Feed extends StatelessWidget {
+class Feed extends StatefulWidget {
   const Feed({
     Key? key,
+    required this.imageUrl // 2. Feed() 호출할 때 값 할당
   }) : super(key: key);
+
+  final String imageUrl; // 1. 이미지를 담을 변수,  3. 생성자 호출 시 받은 이미지를 변수에 저장
+
+  @override
+  State<Feed> createState() => _FeedState();
+}
+
+//'_'로 시작하는 이름 : 외부 파일에서 접근할 수 없는 private
+class _FeedState extends State<Feed> {
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +22,7 @@ class Feed extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start, // 정렬
       children: [
         Image.network(
-          "https://cdn2.thecatapi.com/images/kat_7kqBi.png",
+          widget.imageUrl, // 4. 변수를 상태 클래스에서 접근 (wiget.변수명)
           height: 400,
           width: double.infinity,
           fit: BoxFit.cover, // 이미지의 비율을 유지하면서 고정된 폭과 높이에 맞추어 이미지를 자른다
