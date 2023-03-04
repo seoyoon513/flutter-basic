a81a6aa7342f771440adb34e05548ab251c4d669
seoyoon513 <syoonlee513@gmail.com>
2023-03-04 23:10:10 +0900

    [feat] #5 홈페이지 구현

--- onboarding/lib/main.dart
+++ onboarding/lib/main.dart
@@ -24,37 +24,18 @@ class OnboardingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: IntroductionScreen(
-          pages: [
-            // 페이지 1
-            PageViewModel(
-                title: "빠른 개발",
-                body: "Flutter의 hot reload는 쉽고 UI 빌드를 도와줍니다.",
-                image: Padding(
-                  padding: const EdgeInsets.all(32),
-                  child: Image.network(
-                    "https://user-images.githubusercontent.com/26322627/143761841-ba5c8fa6-af01-4740-81b8-b8ff23d40253.png",
-                  ),
-                ),
-                decoration: PageDecoration(
-                    titleTextStyle: TextStyle(
-                      color: Colors.blueAccent,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    bodyTextStyle: TextStyle(
-                      color: Colors.black,
-                      fontSize: 18,
-                    )
-                )
+      pages: [
+        // 페이지 1
+        PageViewModel(
+            title: "빠른 개발",
+            body: "Flutter의 hot reload는 쉽고 UI 빌드를 도와줍니다.",
+            image: Padding(
+              padding: const EdgeInsets.all(32),
+              child: Image.network(
+                "https://user-images.githubusercontent.com/26322627/143761841-ba5c8fa6-af01-4740-81b8-b8ff23d40253.png",
+              ),
             ),
-
-            // 페이지 2
-            PageViewModel(
-              title: "표현력 있고 유연한 UI",
-              body: "Flutter에 내장된 아름다운 위젯들로 사용자를 기쁘게 하세요.",
-              image: Image.network(
-                  'https://user-images.githubusercontent.com/26322627/143762620-8cc627ce-62b5-426b-bc81-a8f578e8549c.png'),
-              decoration: PageDecoration(
+            decoration: PageDecoration(
                 titleTextStyle: TextStyle(
                   color: Colors.blueAccent,
                   fontSize: 24,
@@ -63,23 +44,37 @@ class OnboardingPage extends StatelessWidget {
                 bodyTextStyle: TextStyle(
                   color: Colors.black,
                   fontSize: 18,
-                ),
-              ),
-            ),
-          ],
+                ))),
 
-          // 필수 파라미터
-          next: Text("Next"),
-          done: Text("Done"),
-          onDone: () {
-            // when done button is pressed
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => HomePage())
-            );
-          },
-        )
-    );
+        // 페이지 2
+        PageViewModel(
+          title: "표현력 있고 유연한 UI",
+          body: "Flutter에 내장된 아름다운 위젯들로 사용자를 기쁘게 하세요.",
+          image: Image.network(
+              'https://user-images.githubusercontent.com/26322627/143762620-8cc627ce-62b5-426b-bc81-a8f578e8549c.png'),
+          decoration: PageDecoration(
+            titleTextStyle: TextStyle(
+              color: Colors.blueAccent,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+            bodyTextStyle: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+            ),
+          ),
+        ),
+      ],
+
+      // 필수 파라미터
+      next: Text("Next"),
+      done: Text("Done"),
+      onDone: () {
+        // when done button is pressed
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => HomePage()));
+      },
+    ));
   }
 }
 
@@ -88,7 +83,18 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page!"),
+      ),
+      body: Center(
+        child: Text(
+          "환영합니다!",
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+      ),
+    );
   }
 }
-
