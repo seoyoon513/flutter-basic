88af890208e0826f1a1f761d12c6c15a75740237
seoyoon513 <syoonlee513@gmail.com>
2023-03-04 23:03:48 +0900

    [feat] #5 온보딩 페이지 구현

--- onboarding/lib/main.dart
+++ onboarding/lib/main.dart
@@ -27,15 +27,45 @@ class OnboardingPage extends StatelessWidget {
         pages: [
           // 페이지 1
           PageViewModel(
-            title: "title1",
-            bodyWidget: Text("body1")
+            title: "빠른 개발",
+            body: "Flutter의 hot reload는 쉽고 UI 빌드를 도와줍니다.",
+            image: Padding(
+              padding: const EdgeInsets.all(32),
+              child: Image.network(
+                  "https://user-images.githubusercontent.com/26322627/143761841-ba5c8fa6-af01-4740-81b8-b8ff23d40253.png",
+              ),
+            ),
+            decoration: PageDecoration(
+              titleTextStyle: TextStyle(
+                color: Colors.blueAccent,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              bodyTextStyle: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+              )
+            )
           ),
 
           // 페이지 2
           PageViewModel(
-            title: "title2",
-            bodyWidget: Text("body2")
-          )
+            title: "표현력 있고 유연한 UI",
+            body: "Flutter에 내장된 아름다운 위젯들로 사용자를 기쁘게 하세요.",
+            image: Image.network(
+                'https://user-images.githubusercontent.com/26322627/143762620-8cc627ce-62b5-426b-bc81-a8f578e8549c.png'),
+            decoration: PageDecoration(
+              titleTextStyle: TextStyle(
+                color: Colors.blueAccent,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              bodyTextStyle: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+              ),
+            ),
+          ),
         ],
 
         // 필수 파라미터
