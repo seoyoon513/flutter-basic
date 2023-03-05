cc5c7bf997878bfaa94800ffb11d9d3f3edd9cb7
seoyoon513 <syoonlee513@gmail.com>
2023-03-05 10:27:48 +0900

    [feat] #7 Row 위젯으로 리스트 아이템 구현

--- bucket_list/lib/main.dart
+++ bucket_list/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,25 +25,46 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<String> bucketlist = ['여행가기']; // 전체 버킷 리스트 목록
+  List<String> bucketlist = ['여행가기', '밥먹기', '잠자기']; // 전체 버킷 리스트 목록
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('버킷 리스트'),
-        ),
-        body: bucketlist.isEmpty // 버킷 리스트 상태에 따라 다른 결과 보여주기
-            ? Center(child: Text('버킷 리스트를 작성해 주세요'))
-            : Center(child: Text('버킷 리스트가 존재합니다!')),
-        floatingActionButton: FloatingActionButton(
-        onPressed: ()
-    {
-      Navigator.push(
-          context, MaterialPageRoute(builder: (_) => CreatePage()));
-    },
-    child: Icon(Icons.add),
-    ),
+      appBar: AppBar(
+        title: Text('버킷 리스트'),
+      ),
+      body: bucketlist.isEmpty // 버킷 리스트 상태에 따라 다른 결과 보여주기
+          ? Center(child: Text('버킷 리스트를 작성해 주세요'))
+          : ListView.builder(
+              itemCount: bucketlist.length,
+              itemBuilder: (BuildContext context, int index) {
+                String bucket = bucketlist[index];
+                return Padding( // Row로 구현
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(bucket,
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),),
+                      IconButton(
+                        padding: EdgeInsets.zero, // 아이콘 기본 패딩값 없애기
+                        onPressed: () {  },
+                        icon: Icon(CupertinoIcons.delete, color: Colors.grey,),
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (_) => CreatePage()));
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
